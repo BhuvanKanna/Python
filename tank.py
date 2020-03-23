c70b7db54b0f07f480c0f397712fe394047b8141
Bhuvan Kanna <39105697+MathMaster123@users.noreply.github.com>
2020-03-23 11:46:46 -0500

    Update tank.py

--- tank.py
+++ tank.py
@@ -1,18 +1,21 @@
 import turtle
 
 wn = turtle.Screen()
-wn.title('Tank')
+#wn.title('Tank')
 wn.setup(width = 600, height = 600)
 wn.tracer(0)
 wn.bgcolor('white')
-image="fighterjet.gif"
 
-wn.register_shape(image)
-
+s = turtle.Shape('compound')
+poly1 = ((0,0),(10,-5),(0,10),(-10,-5))
+s.addcomponent(poly1, "red", "blue")
+poly2 = ((0,0),(10,-5),(-10,-5))
+s.addcomponent(poly2, "blue", "red")
+wn.register_shape(s)
 tank = turtle.Turtle()
 tank.speed(0)
-tank.shapesize(2)
-tank.shape(image)
+tank.turtlesize(2)
+tank.shape(s)
 tank.color("green")
 tank.home()
 tank.up()
@@ -23,7 +26,7 @@ bullet.penup()
 bullet.speed(1)
 bullet.color('yellow')
 bullet.shape('circle')
-bullet.shapesize(.5, .5)
+bullet.turtlesize(.5, .5)
 bullet.hideturtle()
 
 def goright():
@@ -42,6 +45,6 @@ wn.onkeypress(shoot, 'space')
 wn.onkeypress(forward, 'Up')
 wn.listen()
 while True:
-    wn.ontimer(bullet.forward(10), 10)
-#    wn.ontimer(tank.forward(5), 10)
     wn.update()
+    wn.ontimer(tank.forward(5), 10)
+    wn.ontimer(bullet.forward(10), 10)
